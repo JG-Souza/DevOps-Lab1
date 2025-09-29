f8ce7e842a5233cf3c4ab1cb0c7af425216926cb
JG-Souza <joaogsouzafma@gmail.com>
2025-09-29 09:54:37 -0300

    style: formatar arquivo

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-lab1" # Bucket onde o estado será salvo
+    bucket = "tf-state-lab1"          # Bucket onde o estado será salvo
     key    = "lab1/terraform.tfstate" # Caminho do arquivo de estado dentro do bucket
     region = "us-east-2"
   }
